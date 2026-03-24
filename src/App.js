8cd20c2979baf87902a570568db633f842eb6d21
alexa4ij <alexadominic898@gmail.com>
2026-03-24 17:20:19 +0700

    Delete src/App.js

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import React from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import Swap from "./Swap";
-export default function App() {
-    return (_jsxs("div", { style: { padding: 20 }, children: [_jsx("h1", { children: "\uD83D\uDE80 Simple Jupiter DEX" }), _jsx(WalletMultiButton, {}), _jsx(Swap, {})] }));
-}
